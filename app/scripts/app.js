2ea8a16ae4e6ae1d90a21cbf258676505badabbb
Adam Fluke <fluke.adam@gmail.com>
2016-02-16 22:25:50 -0800

    Create yoeman generated scaffold for angular application

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name smartText2App
+ * @description
+ * # smartText2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('smartText2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
